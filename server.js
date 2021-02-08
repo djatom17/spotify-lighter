b425a8a1e775012976982a308af09c99da4f1648
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2021-02-08 17:33:25 +1100

    handle non-existent server routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var createError = require("http-errors");
 var cors = require("cors");
 var stringify = require("querystring").stringify;
 var cookieParser = require("cookie-parser");
@@ -50,87 +51,9 @@ app.get("/login", (req, res) => {
   );
 });
 
-app.get("/callback", (req, res) => {
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[STATE_KEY] : null;
-
-  if (state === null || state !== storedState) {
-    res.redirect(
-      "/#" +
-        stringify({
-          error: "state_mismatch",
-        })
-    );
-  } else {
-    res.clearCookie(STATE_KEY);
-    var auth_form = stringify({
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: REDIRECT_URI,
-    });
-
-    var auth_config = {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization:
-          "Basic " +
-          new Buffer.from(
-            CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET
-          ).toString("base64"),
-      },
-    };
-
-    axios
-      .post("https://accounts.spotify.com/api/token", auth_form, auth_config)
-      .then((response) => {
-        if (response.data) {
-          // !error
-          var access_token = response.data.access_token,
-            refresh_token = response.data.refresh_token;
-          axios
-            .get("https://api.spotify.com/v1/me", {
-              headers: { Authorization: "Bearer " + access_token },
-            })
-            .then((response) => {
-              console.log("User authentication successful.");
-              res.send(response.data);
-            })
-            .catch((err) => res.send(err));
-        } else {
-          res.redirect("/#" + stringify({ error: "invalid_token" }));
-        }
-      })
-      .catch((err) => res.send(err));
-  }
-});
-
-app.get("/refresh-token", (req, res) => {
-  var refresh_token = req.query.refresh_token;
-  axios
-    .post(
-      "https://accounts.spotify.com/api/token",
-      { grant_type: "refresh_token", refresh_token: refresh_token },
-      {
-        headers: {
-          Authorization:
-            "Basic " +
-            new Buffer.from(
-              client_id + ":" + process.env.SPOTIFY_CLIENT_SECRET
-            ).toString("base64"),
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    )
-    .then((response) => {
-      if (response.data) {
-        var access_token = response.data.access_token;
-        res.send({
-          access_token: access_token,
-        });
-      }
-    })
-    .catch((err) => console.log(err));
+// Handle 404 errors
+app.use((_0, _1, next) => {
+  return next(createError(404, "URL not found on this server."));
 });
 
 // Listen for requests
