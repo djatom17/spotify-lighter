b0a296a6b7f877f80b28b6d48c1649009471e591
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2021-02-17 12:11:16 +1100

    use cookies to store tokens from API provisionally

--- api/auth/spotify.js
+++ api/auth/spotify.js
@@ -1,6 +1,7 @@
 var spotifyAuth = require("express").Router();
 var stringify = require("querystring").stringify;
 var axios = require("axios").default;
+const { session } = require("electron");
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -8,6 +9,7 @@ dotenv.config();
 const CLIENT_ID = "f04e4f0caee14520b4e74c9e82b710f4";
 const REDIRECT_URI = "http://localhost:8888/auth/spotify/callback/";
 const STATE_KEY = "spotify_auth_state";
+const TIME_TO_EXPIRE = 1209600; // 2 weeks
 
 /**
  * Generates a random string containing numbers and letters
@@ -55,13 +57,13 @@ spotifyAuth.get("/callback", (req, res) => {
     );
   } else {
     res.clearCookie(STATE_KEY);
-    var auth_form = stringify({
+    var authForm = stringify({
       grant_type: "authorization_code",
       code: code,
       redirect_uri: REDIRECT_URI,
     });
 
-    var auth_config = {
+    var authConfig = {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         Authorization:
@@ -73,19 +75,54 @@ spotifyAuth.get("/callback", (req, res) => {
     };
 
     axios
-      .post("https://accounts.spotify.com/api/token", auth_form, auth_config)
+      .post("https://accounts.spotify.com/api/token", authForm, authConfig)
       .then((response) => {
         if (response.data) {
           // !error
-          var access_token = response.data.access_token,
-            refresh_token = response.data.refresh_token;
+          var accessToken = response.data.access_token,
+            refreshToken = response.data.refresh_token;
           axios
             .get("https://api.spotify.com/v1/me", {
-              headers: { Authorization: "Bearer " + access_token },
+              headers: { Authorization: "Bearer " + accessToken },
             })
             .then((response) => {
               console.log("User authentication successful.");
-              res.send(response.data);
+
+              var expiry = Date.now() / 1000 + TIME_TO_EXPIRE;
+
+              const accessCookie = {
+                url: "https://www.spotify.com",
+                name: "spotifyAccessToken",
+                value: accessToken,
+                secure: true,
+                expirationDate: expiry,
+              };
+
+              const refreshCookie = {
+                url: "https://www.spotify.com",
+                name: "spotifyRefreshToken",
+                value: refreshToken,
+                secure: true,
+                expirationDate: expiry,
+              };
+
+              session.defaultSession.cookies.set(accessCookie).then(
+                () => {
+                  console.log("Saved access token to cookie.");
+                },
+                (error) => {
+                  console.error("Error saving access token to cookie", error);
+                }
+              );
+
+              session.defaultSession.cookies.set(refreshCookie).then(
+                () => {
+                  console.log("Saved refresh token to cookie.");
+                },
+                (error) => {
+                  console.error("Error saving refresh token to cookie", error);
+                }
+              );
             })
             .catch((err) => res.send(err));
         } else {
@@ -97,31 +134,44 @@ spotifyAuth.get("/callback", (req, res) => {
 });
 
 spotifyAuth.get("/refresh-token", (req, res) => {
-  var refresh_token = req.query.refresh_token;
-  axios
-    .post(
-      "https://accounts.spotify.com/api/token",
-      { grant_type: "refresh_token", refresh_token: refresh_token },
-      {
-        headers: {
-          Authorization:
-            "Basic " +
-            new Buffer.from(
-              client_id + ":" + process.env.SPOTIFY_CLIENT_SECRET
-            ).toString("base64"),
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    )
-    .then((response) => {
-      if (response.data) {
-        var access_token = response.data.access_token;
-        res.send({
-          access_token: access_token,
-        });
-      }
+  // Obtain refresh token from cookies.
+  // If it is not found or expired, then redirect user to login.
+  session.defaultSession.cookies
+    .get({ url: "https://www.spotify.com", name: "spotifyRefreshToken" })
+    .then((cookies) => {
+      var refreshToken = cookies[0].value;
+      axios
+        .post(
+          "https://accounts.spotify.com/api/token",
+          { grant_type: "refresh_token", refresh_token: refreshToken },
+          {
+            headers: {
+              Authorization:
+                "Basic " +
+                new Buffer.from(
+                  client_id + ":" + process.env.SPOTIFY_CLIENT_SECRET
+                ).toString("base64"),
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        )
+        .then((response) => {
+          if (response.data) {
+            var accessToken = response.data.access_token;
+            res.send({
+              access_token: accessToken,
+            });
+          }
+        })
+        .catch((err) => console.log(err));
     })
-    .catch((err) => console.log(err));
+    .catch((error) => {
+      console.error(
+        "Sorry, refresh token not found. Please login again.",
+        error
+      );
+      res.redirect("/auth/spotify/login");
+    });
 });
 
 module.exports = spotifyAuth;
